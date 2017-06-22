8b7ff545e1c6ce680882c4c9d0c1b730031d3d61
Jonatas Leon <jonatasleon@dallas.dgi.inpe.br>
2017-06-22 09:49:57 -0300

    Add piece model

---
+++ models/piece.js
@@ -0,0 +1,39 @@
+const db = require('./neo4j');
+const { mapRecord, defaultCatch: _defaultCatch } = require('./helpers');
+
+const defaultCatch = _defaultCatch(db);
+
+const label = 'piece ';
+const name = 'Piece';
+
+const createStmt = `CREATE (${ label }:${ name } { alias: { alias } }),
+  (${ label })-[:CLOSES_WITH]->(clasp),
+  (${ label })-[:USES]->(cord)<-[:ATTACHES_TO]-(cordStone),
+  (pendantStone)-[:FITS_IN]->(pendant)-[:IS_IN]->(position1)
+WITH ${ label }
+MATCH r=(${ label })-[:CONTAINS|CLOSES_WITH|USES*1..3]-()
+RETURN r`;
+const getAllStmt = `MATCH r=(:${ name })-[:CONTAINS|CLOSES_WITH|USES*1..3]-()
+RETURN r`;
+const deleteAllStmt = `MATCH (${ label }:${ name }) DETACH DELETE ${ label }`;
+
+module.exports = {
+  create: piece => db.run( createStmt, piece)
+      .then((result) => {
+        db.close();
+        const data = result.records.map(mapRecord(label));
+        return data.pop();
+      })
+      .catch(defaultCatch),
+  getAll: () => db.run(getAllStmt)
+      .then((result) => {
+        db.close();
+        const data = result.records.map(mapRecord(label));
+        return data;
+      }),
+  deleteAll: () => db.run(deleteAllStmt)
+      .then(() => {
+        db.close();
+      })
+      .catch(defaultCatch),
+};

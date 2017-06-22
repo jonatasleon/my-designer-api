f90a4f2c97195b479c912459c2cfab5989f147a6
Jonatas Leon <jonatasleon@dallas.dgi.inpe.br>
2017-06-22 09:10:44 -0300

    Update import statement

--- models/neo4j.js
+++ models/neo4j.js
@@ -1,5 +1,7 @@
-const { v1: neo4j } = require('neo4j-driver');
-const debug = require('debug')('my-designer:model');
+import { v1: neo4j } from 'neo4j-driver'
+import { debug: _debug } = from 'debug'
+
+debug = _debug('my-designer:model');
 
 const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'root'));
 

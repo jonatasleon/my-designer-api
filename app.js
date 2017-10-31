f034c23af69ef9bded08b4fef25307835c7f58f9
Jonatas Leon <jonatas.leon@gmail.com>
2017-10-31 07:03:44 -0200

    organizing files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import path from 'path';
+import path from 'path';
 // import favicon from 'serve-favicon';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
@@ -10,10 +10,14 @@ import middlewares from './middlewares';
 const app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'pug');
+
 
 app.use('/api', routes());
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ export default () => {
   const router = new express.Router();
 
   router.get('/', (req, res) => {
-    res.json({ title: 'Express' });
+    res.render('index');
   });
 
   return router;
